a8933d635b8d85e822f4b315b04314997c31e767
Marsel Khalikov <mhnsa@mail.ru>
2021-08-02 21:39:16 +0300

    Light refactoring

--- src/main/java/com/example/number_generator/service/CarNumberServiceImpl.java
+++ src/main/java/com/example/number_generator/service/CarNumberServiceImpl.java
@@ -9,18 +9,64 @@ import java.util.Set;
 
 @Service
 public class CarNumberServiceImpl implements CarNumberService {
-    
-    Set<String> carNumbersDB = new HashSet<>();
-    char[] carLetters = {'А', 'В', 'Е', 'К', 'М', 'Н', 'О', 'Р', 'С', 'Т', 'У', 'Х'};
-    int number;
 
-    int firstLetterIndex;
-    int secondLetterIndex;
-    int thirdLetterIndex;
-    final String REGION = " 116 RUS";
+    Set<String> issuedCarNumbers = new HashSet<>();
+    char[] carLetters = {'А', 'В', 'Е', 'К', 'М', 'Н', 'О', 'Р', 'С', 'Т', 'У', 'Х'};
+    private int number;
+
+    private int firstLetterIndex;
+    private int secondLetterIndex;
+    private int thirdLetterIndex;
+    private final String REGION = " 116 RUS";
 
     String lastIssuedNumber = null;
     Random generator = new Random();
+
+    @Override
+    public String generateRandomCarNumber() {
+        String randomCarNumber = "" + generateFirstLetter() + generateNumber() + generateSecondLetter() + generateThirdLetter() + REGION;
+        lastIssuedNumber = randomCarNumber;
+        if (issuedCarNumbers.add(randomCarNumber)) {
+            return randomCarNumber;
+        } else {
+           return generateRandomCarNumber();
+        }
+    }
+
+    @Override
+    public String generateNextCarNumber() {
+        if (number != 999) {
+            number++;
+        } else if (thirdLetterIndex != carLetters.length) {
+            number = 0;
+            thirdLetterIndex++;
+        } else if (secondLetterIndex != carLetters.length) {
+            thirdLetterIndex = 0;
+            secondLetterIndex++;
+        } else if (firstLetterIndex != carLetters.length) {
+            thirdLetterIndex = 0;
+            secondLetterIndex = 0;
+            firstLetterIndex++;
+        } else {
+            thirdLetterIndex = 0;
+            secondLetterIndex = 0;
+            firstLetterIndex = 0;
+        }
+        String nextNumber = "" + carLetters[firstLetterIndex] + convertNumberToString(number) +
+        carLetters[secondLetterIndex] + carLetters[thirdLetterIndex] + REGION;
+
+        if (issuedCarNumbers.add(nextNumber)) {
+            return nextNumber;
+        } else {
+            System.out.println("This car number has been already issued");
+            return generateRandomCarNumber();
+        }
+    }
+
+    @Override
+    public Set<String> showAll() {
+        return issuedCarNumbers;
+    }
 
     public char generateFirstLetter() {
         firstLetterIndex = generator.nextInt(carLetters.length);
@@ -50,50 +96,5 @@ public class CarNumberServiceImpl implements CarNumberService {
         } else {
             return String.format("%d", this.number);
         }
-    }
-
-    @Override
-    public String generateRandomCarNumber() {
-        String randomCarNumber = "" + generateFirstLetter() + generateNumber() + generateSecondLetter() + generateThirdLetter() + REGION;
-        lastIssuedNumber = randomCarNumber;
-        if (carNumbersDB.add(randomCarNumber)) {
-            return randomCarNumber;
-        } else {
-           return generateRandomCarNumber();
-        }
-    }
-
-    @Override
-    public String generateNextCarNumber() {
-        if (number != 999) {
-            number++;
-        } else if (thirdLetterIndex != carLetters.length) {
-            number = 0;
-            thirdLetterIndex++;
-        } else if (secondLetterIndex != carLetters.length) {
-            thirdLetterIndex = 0;
-            secondLetterIndex++;
-        } else if (firstLetterIndex != carLetters.length) {
-            thirdLetterIndex = 0;
-            secondLetterIndex = 0;
-            firstLetterIndex++;
-        } else {
-            thirdLetterIndex = 0;
-            secondLetterIndex = 0;
-            firstLetterIndex = 0;
-        }
-        String nextNumber = "" + carLetters[firstLetterIndex] + convertNumberToString(number) +
-        carLetters[secondLetterIndex] + carLetters[thirdLetterIndex] + REGION;
-
-        if (carNumbersDB.add(nextNumber)) {
-            return nextNumber;
-        } else {
-            return generateRandomCarNumber();
-        }
-    }
-
-    @Override
-    public Set<String> showAll() {
-        return carNumbersDB;
     }
 }
